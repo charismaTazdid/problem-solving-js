41a019e55e9e581218a6d769c816cb2fe153a317
raihan tajdid <raihantajdid@gmail.com>
2023-03-19 05:05:58 +0600

    remove Not Applicable value from object

---
+++ loopOverObject.js
@@ -0,0 +1,31 @@
+// Goal is to loop over an object and check something valid
+
+const song = {
+    name: "niscup Adhare", band: "Shironamhin",
+    album: "jahazi", vocal: 'Tanzir tuhin', year: 'N/A',
+    gener: "Rock", writer: "N/A"
+}
+
+const keys = Object.keys(song)
+const values = Object.values(song)
+
+// trying to get result "name = value" format
+for (i = 0; i < values.length; i++) {
+    // console.log(keys[i] + " = " + values[i])
+}
+
+// write a function to remove "N/A" from an object
+
+const removeNA = (obj) => {
+    const keys = Object.keys(obj);
+    const values = Object.values(obj);
+
+    for (let i = 0; i < values.length; i++) {
+        if (values[i] == "N/A") {
+            delete obj[keys[i]]
+        }
+    }
+    return obj;
+};
+
+console.log(removeNA(song))
